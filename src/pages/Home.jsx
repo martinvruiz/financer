2ca5819ad6138662a3f89e9a50d8c76402d4b491
¨martinvruiz¨ <martinvruiz10@gmail.com>
2024-12-10 13:40:46 -0300

    added non data page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,29 @@ import { TransactionForm } from "../components/TransactionForm/TransactionForm"
 import { TransactionList } from "../components/TransactionList/TransactionList"
 import { TotalTransactions } from "../components/TotalTransactions/TotalTransactions"
 import { useState } from "react"
+import { useTransationContext } from "../context/TransactionContext";
 
 
 
 
 export const Home = ()=>{
+    const { transactions } = useTransationContext() 
+
+
     const [selectedDate, setSelectedDate] = useState("");
     
     return <>
         <h1 className="my-2 p-2 text-4xl">Financer</h1>
         <p className="mb-2 w-3/4 text-center">Te ayudamos a manejar tus finanzas, introducí en el formulario el tipo de ingreso o gasto</p>
         <TransactionForm/>
-        <h2 className="my-2 py-1 text-2xl">Lista de Ingresos/Gastos</h2>
-        <MonthSelector setSelectedDate={setSelectedDate}/>
-        <TotalTransactions selectedDate={selectedDate}/>
-        <TransactionList selectedDate={selectedDate}/>
+        <h2 className="my-2 py-1 text-2xl font-bold">Lista de Ingresos/Gastos</h2>
+        {(transactions.length === 0 ? (
+            <p className="p-2 my-2 text-xl">No hay datos para mostrar!</p>) : (
+            <div className="w-full flex flex-col items-center">
+                <MonthSelector setSelectedDate={setSelectedDate}/>
+                <TotalTransactions selectedDate={selectedDate}/>
+                <TransactionList selectedDate={selectedDate}/>
+        </div>
+        ))}
     </>
 }
