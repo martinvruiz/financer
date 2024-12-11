14a207c86e3dd7c436f632d0e4e91e056dff612e
¨martinvruiz¨ <martinvruiz10@gmail.com>
2024-12-11 16:33:13 -0300

    fixed font issues

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -39,11 +39,11 @@ export const TransactionList = ({ selectedDate })=>{
                         className="flex justify-between items-center p-4 bg-white shadow-md rounded-lg border border-gray-200 hover:shadow-lg transition-shadow" 
                         key={transaction.id}>
                             <p 
-                                className="text-sm md:text-base">
+                                className="text-xs md:text-base">
                                     {formatDate(transaction.date)}
                             </p>
                             <p 
-                                className="w-1/4 text-sm md:text-base text-indigo-600 font-medium">
+                                className="w-1/4 text-xs md:text-base text-indigo-600 font-medium">
                                     {formatCategory(transaction.category)}
                             </p>
                             <p 
@@ -56,7 +56,7 @@ export const TransactionList = ({ selectedDate })=>{
                             </p>
                             <button 
                                 onClick={()=>deleteTransaction(transaction.id)} 
-                                className="p-2 bg-red-500 text-white rounded-md shadow hover:bg-red-600 transition-colors">
+                                className="p-2 bg-red-500 text-white text-xs md:text-base rounded-md shadow hover:bg-red-600 transition-colors">
                                     Eliminar
                             </button>
                     </li>
